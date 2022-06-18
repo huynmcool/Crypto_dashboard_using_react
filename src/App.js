deb4aa255eab386442ba570fffa222c0ff70b3f5
huynmcool <huynmcool@gmail.com>
2022-06-18 21:56:15 +0700

    add grid system for layout

--- src/App.js
+++ src/App.js
@@ -1,70 +1,27 @@
-import React, { useRef } from 'react';
+import React from 'react';
+import "primeflex/primeflex.css";
 import './App.css';
-import { Sidebar } from 'primereact/sidebar';
-import { Button } from 'primereact/button';
-import { Menu } from 'primereact/menu';
-import { Toast } from 'primereact/toast';
 
 const App = () => {
-  const menu = useRef(null);
-  const toast = useRef(null);
-  const logo = <img alt="logo" src="showcase/images/logo.png" onError={(e) => e.target.src = 'https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png'} height="40" className="mr-2"></img>;
-
-  const items = [
-    {
-      label: 'Sidebar',
-      items: [
-        {
-          label: 'Logo',
-          icon: logo,
-          command: () => {
-            toast.current.show({ severity: 'success', summary: 'Updated', detail: 'Data Updated', life: 3000 });
-          }
-        },
-        {
-          label: 'Update',
-          icon: 'pi pi-refresh',
-          command: () => {
-            toast.current.show({ severity: 'success', summary: 'Updated', detail: 'Data Updated', life: 3000 });
-          }
-        },
-        {
-          label: 'Delete',
-          icon: 'pi pi-times',
-          command: () => {
-            toast.current.show({ severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000 });
-          }
-        }
-      ]
-    },
-    {
-      label: 'Navigate',
-      items: [
-        {
-          label: 'React Website',
-          icon: 'pi pi-external-link',
-          url: 'https://reactjs.org/'
-        },
-        {
-          label: 'Router',
-          icon: 'pi pi-upload',
-          command: (e) => {
-            window.location.hash = "/fileupload"
-          }
-        }
-      ]
-    }
-  ];
-
   return (
-    <div>
-      <Toast ref={toast}></Toast>
-      <div className="card">
-        
-      </div>
-      <div className="layout-sidebar">
-        <h5>Inline</h5>
-        <Menu className='sidebar' model={items} />
+    <div className='app'>
+      <div class="grid h-screen w-screen grid-nogutter overflow-hidden">
+        <div class="col-.5 bg-blue-500 text-white font-bold flex align-items-center justify-content-center border-round" >
+          Left sidebar
+        </div>
+        <div class="col bg-yellow-500 text-white font-bold flex border-round align-items-center justify-content-center" >
+          <div class="grid w-full h-full overflow-hidden align-items-center justify-content-center grid-nogutter">
+            <div class="col-1  w-full h-4rem bg-red-500 text-white font-bold flex border-round align-items-center justify-content-center">
+              Topbar
+            </div>
+            <div class="col w-full h-full bg-green-500 text-white font-bold flex border-round align-items-center justify-content-center">
+              Content 2
+            </div>
+          </div>
+        </div>
+        <div class="col-3 bg-purple-500 text-white font-bold flex align-items-center justify-content-center border-round">
+          Right sidebar
+        </div>
       </div>
     </div>
   );
